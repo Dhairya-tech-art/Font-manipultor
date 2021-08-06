a321c4f9b8c1dadf8187e289c5d5a8bd9094805c
Dhairya-tech-art <74977651+Dhairya-tech-art@users.noreply.github.com>
2021-08-06 22:01:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-noseX=0;
-noseY=0;
-difference=0;
-rightWristX=0;
-leftWristX=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-function setup() {
-    canvas=createCanvas(600,500)
-    canvas.center();
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-    video=createCapture(VIDEO);
-    video.hide();
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
 
-    poseNet= ml5.poseNet(video, modelLoaded);
-    poseNet.on ('pose',gotPoses);
-
-    function modelLoaded(){
-        console.log ('PoseNet Is Intialized');
-    }
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function draw() {
-    image(video, 0, 0, 600, 500)
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
 }
+
 
 function gotPoses(results)
 {
-    if(results.length>0)
-    {
-        console.log(results);
-        noseX= results[0].pose.nose.x;
-        noseY= results[0].pose.nose.y;
-        console.log("noseX="+ noseX+"noseY="+noseY);
+  if(results.length > 0)
+  {
+    console.log(results);
 
-        console.log(results);
-        leftWristX= results[0].leftWrist.x;
-        rightWristY= results[0].pose.rightWrist.x;
-        diffrence= floor(leftWristX - rightWristX);
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 
-        console.log("leftWristX="+ leftWristX +"rightWristX="+rightWristX+"difference="+difference);
-    }
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
+function draw() {
+background('#6C91C2');
+
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Dhairya', 50, 400);
+}
